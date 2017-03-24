64a6201aa7ea0a3efef84ad57ec8d3065e6f543e
edwinbilla <imaprogrammer@icloud.com>
2017-03-23 19:29:26 -0500

    Removed jQuery

--- js/script.js
+++ js/script.js
@@ -1,163 +1,166 @@
 //jshint esversion:6
 
+const studentListContainer = document.getElementsByClassName('student-list')[0];
+const studentItems = document.getElementsByClassName('student-item');
+const studentListItems = [].slice.call(studentItems);
+// calculates up to first 10 students and stores in array studentsToShow
+let studentsToShow = studentListItems.slice(0,10);
+const numberOfPages = Math.ceil(studentListItems.length / 10);
+const pageDiv = document.getElementsByClassName('page')[0];
 
-
-/************************************ Functions **************************************/
-
-//function that generates and Prints message to the  div with class student-list
-let showMessage = (message) => {
-	const messageParagraph = document.createElement('p');
-	messageParagraph.className = 'notFound';
-	messageParagraph.textContent = message;
-	$studentList.append(messageParagraph);
-}
-//function that removes Items from the div with class student-list
-let removeStudentListItems = () => {
-	while ($studentList[0].firstChild) {
-		$studentList[0].firstChild.remove();
+//removes the children of htmlCollections
+const removeChildren = (htmlCollections)=>{
+	while(htmlCollections.firstChild){
+		htmlCollections.removeChild(htmlCollections.firstChild);
 	}
-}
-
-//function to enhance paginations Links
-let paginationMechanism = (arrayLike) => {
-	//add event handler to the ul
-	paginationUl.addEventListener('click', (event) => {
-		if (event.target.tagName == 'A') {
-			//remove the student list items from the dom
-			removeStudentListItems();
-			//get the page number and stores in variable
-			const pageNumber = event.target.textContent;
-			//checks pageNumber and appends the right number of items
-			if (pageNumber === '1') {
-				//gets first 10  list items
-				itemsToShow = arrayLike.slice(0, 10);
-				//appends the first 10  list items to the div with class student-list
-				$studentList.append(itemsToShow);
-			}
-			else {
-				//gets the computed value of number of  list items for each page
-				itemsToShow = arrayLike.slice(((pageNumber * 10) - 10), (pageNumber * 10));
-				//appends the first 10  list items to the div with class student-list
-				$studentList.append(itemsToShow);
-			}
-			/****iterate through the pagination links and checks 
-			if equal to page number to add classname/remove ****/
-			for (let i = 0; i < numberOfPages; i++) {
-				if (paginationUl.children[i].firstChild.textContent === pageNumber) {
-					paginationUl.children[i].firstChild.classList.add('active');
-				}
-				else {
-					paginationUl.children[i].firstChild.classList.remove('active');
-				}
-			}
-		}
-	});
-}
-
-//iterate through pagination links and hide excess unneccessary pagination links
-let hideExcessPaginationLinks = () => {
-	for (let i = pagesForMatchedSearch; i < numberOfPages; i++) {
-		paginationUl.children[i].firstChild.style.display = 'none';
+};
+// appends items in arrayLike to parent as children
+const appendChildren = (arrayLike, parent)=> {
+	for(let i = 0; i < arrayLike.length; i++){
+		parent.appendChild(arrayLike[i]);
 	}
-}
-
-/************************************ End of Functions **************************************/
-
-
-const $studentList = $('.student-list');		//selects the ul that contains the student list items
-const $studentListItems = $('.student-item');		//selects the students list items in the ul with class student list
-const numberOfItems = $studentListItems.length;		//calculates and stores the number of list items in the ul
-let searchMatches = [];		//array to store search matches
-let pagesForMatchedSearch = 0;		//stores the number of pages the searchMatches can occupy
-removeStudentListItems();		//remove the student list items from the dom
-let itemsToShow = $studentListItems.slice(0, 10);	// variable stores first 10 student list items
-$studentList.append(itemsToShow);		//append the first 10 student list items to student-list div
-const numberOfPages = Math.ceil(numberOfItems / 10);	//calculate the number of pages and store in variable
-let paginationLi = '';		//stores the html of the pagination li
-//create Pagination Links
-for (let i = 1; i <= numberOfPages; i++) {
-	paginationLi += '<li><a href="#">';
-	paginationLi += i;
-	paginationLi += '</a></li>';
-}
-//create pagination Div and append content
+};
+//removes the student list items from the ul with class student list
+removeChildren(studentListContainer);
+//and append only first 10 student list items
+appendChildren(studentsToShow, studentListContainer);
+//create pagination
 const paginationDiv = document.createElement('div');
 paginationDiv.className = 'pagination';
 const paginationUl = document.createElement('ul');
 paginationDiv.append(paginationUl);
-paginationUl.innerHTML = paginationLi;		//put the pagination Li as the html inside ul of the pagination Div
-paginationUl.children[0].firstChild.className = 'active';		//add the class active to the first link in the pagination li
-$('.page').append(paginationDiv);		//append the pagination Div into the Dom
-
-paginationMechanism($studentListItems);		//enhance pagination Links
-
-//create search HTML 
-const studentSearchDiv = document.createElement('div');
-studentSearchDiv.className = 'student-search';
-const searchField = document.createElement('input');
-searchField.setAttribute('placeholder', 'Search for students...');
-const searchButtom = document.createElement('button');
-searchButtom.textContent = 'Search';
-studentSearchDiv.append(searchField, searchButtom);
-$('.page-header').append(studentSearchDiv); 	//append the studentSearchDiv into the div with class page-header
-
-//checks and hide pagination link for when number of pages is 1
-if(numberOfPages === 1){
-	paginationUl.children[0].firstChild.style.display = 'none'; // hide the pagination link
+let paginationLi = '';
+for(let i = 1; i <= numberOfPages; i++) {
+	paginationLi += '<li><a href="#">';
+	paginationLi += i;
+	paginationLi += '</a></li>';
 }
+paginationUl.innerHTML = paginationLi;
+//targets the first pagination link and set class to active
+paginationUl.firstElementChild.firstElementChild.className = 'active';
+//append pagination to bottom of Div with class page
+pageDiv.appendChild(paginationDiv);
 
-let namesArray = [];	//array to hold all names
-let emailArray = []; 	//array to hold all emails
+//remove the active class from the pagination links
+const removeActiveClass = () => {
+	for(let i = 0; i < paginationUl.children.length; i++){
+		paginationUl.children[i].firstElementChild.className = '';
+	}
+};
 
-//iterate through the names and emails html collections
-for (let i = 0; i < numberOfItems; i++) {
-	namesArray.push($studentListItems[i].children[0].children[1].textContent);		//pushes the names into the array
-	emailArray.push($studentListItems[i].children[0].children[2].textContent);		//pushes the email into the array
-}
-
-//add event listener to the search searchButtom
-searchButtom.addEventListener('click', () => {
-	itemsToShow = [];		//empty content of itemsToShow
-	searchMatches = [];		//empty content of searhMatches
-	removeStudentListItems(); 	//remove the children of the div with class student-list
-
-	//iterate through the number of list items and push matching into searchMatches
-	for (let i = 0; i < numberOfItems; i++) {
-		//checks if the input value matches any item of the  namesArray or emailArray
-		if (namesArray[i].includes($('.student-search input')[0].value.toLowerCase()) || emailArray[i].includes($('.student-search input')[0].value.toLowerCase())) {
-			searchMatches.push($studentListItems[i]);		//stores the matched result into the searchMatches array
+//function to enhance pagination links
+const enhancePagination = (arrayLike) => {
+	//add event handler to enhance the pagination links
+	paginationUl.addEventListener('click', (event) => {
+		if(event.target.tagName === 'A'){
+			removeChildren(studentListContainer);
+			studentsToShow = [];
+			const pageNumber = parseInt(event.target.textContent);
+			const index = pageNumber - 1;
+			removeActiveClass();
+			// add the active class to the clicked pagination link
+			paginationUl.children[index].firstElementChild.className = 'active';
+			if(pageNumber === 1){
+				//slice first 10 items into studentsToShow
+				studentsToShow = arrayLike.slice(0,10);
+				//append only first 10 student list items
+				appendChildren(studentsToShow, studentListContainer);			
+			}
+			else{
+				//calculate the right slice start and  end for the student items to be printed
+				studentsToShow = arrayLike.slice(((pageNumber * 10)-10), (pageNumber * 10));
+				//appends the  students items to the ul with class student-list
+				appendChildren(studentsToShow, studentListContainer);
+			}
 		}
-	}
+	});
+};
+// enhance pagination links
+enhancePagination(studentListItems);
 
-	pagesForMatchedSearch = Math.ceil(searchMatches.length / 10);		//computed value for the number of pages the searchMatches can occupy
+//create search bar and insert in Dom
+const searchDiv = document.createElement('div');
+searchDiv.className = 'student-search';
+const searchInputElem = document.createElement('input');
+searchInputElem.setAttribute('placeholder', 'Search for students...');
+const searchButton = document.createElement('button');
+searchButton.textContent = 'Search';
+searchDiv.appendChild(searchInputElem);
+searchDiv.appendChild(searchButton);
+const pageHeader = document.getElementsByClassName('page-header')[0];
+//insert searchDiv into the dom
+pageHeader.insertAdjacentElement('beforeend', searchDiv);
 
-	//iterate through the pagination links 
-	for (let i = 0; i < numberOfPages; i++) {
-		paginationUl.children[i].firstChild.style.display = 'inline';		//set the display property to a value of inline for all pagination links
-		paginationUl.children[i].firstChild.classList.remove('active');		//removes the class active from all pagination links
-	}
-
-	paginationUl.children[0].firstChild.className = 'active';		//sets the class active to the first pagination link
-
-	//checks and appends searchMatches to the dom if it's not empty else prints out message not found
-	if (searchMatches.length > 0 && searchMatches.length <= 10) {
-		itemsToShow = searchMatches;
-		$studentList.append(itemsToShow);
-		// paginationUl.children[0].firstChild.style.display = 'none';
-		// hideExcessPaginationLinks();	//hide excess pagination links
-		//hide pagination links if the items is less than 11
+//add a click event handler to the searchDiv
+searchDiv.addEventListener('click', (event)=>{
+	if(event.target.tagName === 'BUTTON'){
+		studentsToShow = []; 	//empty content of studentsToShow
+		//show all pagination  links
 		for(let i = 0; i < numberOfPages; i++){
-			paginationUl.children[i].firstChild.style.display = 'none';
+			paginationUl.children[i].style.display = '';
 		}
-	}
-	else if (searchMatches.length > 10) {
-		itemsToShow = searchMatches.slice(0, 10);
-		$studentList.append(itemsToShow);
-		hideExcessPaginationLinks();	//hide excess pagination links
-		paginationMechanism(searchMatches);		//enhance pagination links
-	}
-	else {
-		hideExcessPaginationLinks();	//hide excess pagination links 
-		showMessage('Your Search Returned No Result(s)!');		//prints to the document
+		const searchInputValue = searchInputElem.value.toLowerCase();
+		let names = [];
+		let emails = [];
+		let searchMatched = [];
+		//Get the name and email list to compare
+		for(let i = 0; i < studentListItems.length; i++){
+			names.push(studentListItems[i].querySelector('h3').textContent);
+			emails.push(studentListItems[i].querySelector('.email').textContent);
+		}
+		//for each name or email compare with search input value and save student item on search matched
+		for(let i = 0; i < studentListItems.length; i++){
+			if (names[i].toLowerCase().includes(searchInputValue) || emails[i].toLowerCase().includes(searchInputValue)){
+				searchMatched.push(studentListItems[i]);
+			}
+		}
+		//gets and stores number of pages for searchMatched
+		const pagesForSearch = Math.ceil(searchMatched.length /10);
+		// hides all unused pagination links
+		const hideUnusedPagination = () => {
+			for(let i = pagesForSearch; i < numberOfPages; i++){
+				paginationUl.children[i].style.display = 'none';
+			}
+		};
+		//hides all pagination links
+		const hideAllPagination = () => {
+			for(let i = 0; i < numberOfPages; i++){
+				paginationUl.children[i].style.display = 'none';
+			}
+		};
+		//checks for the number of search results at a time and enhances
+		if(searchMatched.length >= 1 && searchMatched.length <= 10){
+			console.log(searchMatched);
+			removeChildren(studentListContainer);
+			appendChildren(searchMatched, studentListContainer);
+			//enhance pagination effect on searchMatched
+			enhancePagination(searchMatched);
+			//hide pagination links
+			hideAllPagination();
+		}
+		else if(searchMatched.length > 10){
+			removeChildren(studentListContainer);
+			studentsToShow = searchMatched.slice(0,10);
+			appendChildren(studentsToShow, studentListContainer);
+			removeActiveClass();
+			//add active class to first page number 1
+			paginationUl.children[0].firstElementChild.className = 'active';
+			//enhance pagination effect on searchMatched
+			enhancePagination(searchMatched);
+			//hides unused pagination links
+			hideUnusedPagination();
+		}
+		else{
+			removeChildren(studentListContainer);
+			hideAllPagination();
+			const noResultMessage = document.createElement('p');
+			noResultMessage.textContent = 'Search returned no result(s)!';
+			studentListContainer.appendChild(noResultMessage);
+		}
 	}
 });
+
+
+
+
+
